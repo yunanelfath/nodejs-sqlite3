8223262022cdbc3b25d6f43c24993995c6a1fa6f
yunankf <yunan@nusantara.technology>
2019-11-10 09:46:51 +0700

    save position history

--- src/controllers/trip.controller.js
+++ src/controllers/trip.controller.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const TripRepository = require('../models/trip')
+const PositionRepository = require('../models/position')
 const tripModel = new TripRepository()
+const positionModel = new PositionRepository()
 
 let controller = {
   dumpRecord: function(importFile){
@@ -9,13 +11,33 @@ let controller = {
     var obj = JSON.parse(fs.readFileSync(importFile, 'utf8'));
 
     let values = []
+    let position_values = []
+    let position_creates = []
     console.log(obj.trips.length)
     let fields = [
       'id','start','end','distance','duration','max_speed','average_speed','idle_duration','score'
     ];
+    let position_fields = [
+      'latitude','longitude','speed','voltage','distance','tracked_at','trip_id'
+    ]
+    let position_values_join = []
     for(let i=0;i<obj.trips.length;i++){
-      // debugger
+
       let value = obj.trips[i]
+
+      // build position object values
+      for(c=0;c<value.histories.length;c++){
+
+        let value_history = value.histories[c]
+        position_values[c] = []
+        for(let b=0;b<position_fields.length;b++){
+          let position_push = position_fields[b] !== 'trip_id' ? `'${value_history[position_fields[b]]}'` : `'${value.id}'`
+          position_values[c].push(position_push)
+        }
+      }
+      position_values_join.push(`(${position_values.join('),(')})`)
+
+      // build trips object values
       values[i] = []
       for(let a=0;a<fields.length;a++){
         if(['start','end'].includes(fields[a])){
@@ -26,15 +48,16 @@ let controller = {
       }
 
     }
-    // console.log(values.join('),('))
+
     values = `(${values.join('),(')})`
-    // console.log(values)
+    position_values_join = position_values_join.join(',')
+
     tripModel.create({fields, values})
+    positionModel.create({fields: position_fields, values: position_values_join})
     return values
   },
   get: async function(){
     let data = await tripModel.all()
-    console.log(data)
     return data
   },
   hello: async function(req, res){
--- src/models/position.js
+++ src/models/position.js
@@ -15,9 +15,25 @@ class PositionRepository extends AppDBO  {
         name TEXT,
         latitude varchar(255),
         longitude varchar(255),
+        speed varchar(255),
+        voltage varchar(255),
+        distance varchar(255),
         tracked_at datetime
       )`
     return super.run(sql)
+  }
+
+  create(obj) {
+    const { fields, values } = obj
+
+    return super.run(
+      `INSERT INTO positions (${fields}) VALUES ${values}`)
+  }
+  get(id){
+    return super.get(`select * from positions where id = ?`,[id])
+  }
+  all(){
+    return super.all(`select * from positions order by id desc`)
   }
 }
 
